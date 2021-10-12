20893737c51821d6dd92cdb242bf79a47b60e754
earlgray <earlgray283@gmail.com>
2021-10-13 00:31:15 +0900

    feat: report search option

--- gakujo/report.go
+++ gakujo/report.go
@@ -2,6 +2,7 @@ package gakujo
 
 import (
 	"bytes"
+	"errors"
 	"io"
 	"net/url"
 	"strconv"
@@ -10,8 +11,11 @@ import (
 	"github.com/szpp-dev-team/gakujo-api/scrape"
 )
 
-func (c *Client) ReportRows() ([]model.ReportRow, error) {
-	page, err := c.fetchReportPage()
+func (c *Client) ReportRows(option *model.ReportSearchOption) ([]model.ReportRow, error) {
+	if option.SchoolYear == 0 || option.SemesterCode.Int() == 0 {
+		return nil, errors.New("Some of options must be set")
+	}
+	page, err := c.fetchReportRowsPage(option)
 	if err != nil {
 		return nil, err
 	}
@@ -26,7 +30,25 @@ func (c *Client) ReportDetail(option *model.ReportDetailOption) (model.ReportDet
 	return scrape.ReportDetail(io.NopCloser(bytes.NewReader(page)))
 }
 
-func (c *Client) fetchReportPage() ([]byte, error) {
+func (c *Client) fetchReportRowsPage(option *model.ReportSearchOption) ([]byte, error) {
+	if _, err := c.fetchGeneralPurposeReportPage(); err != nil {
+		return nil, err
+	}
+	data := url.Values{}
+	data.Set("schoolYear", strconv.Itoa(option.SchoolYear))
+	data.Set("semesterCode", strconv.Itoa(option.SemesterCode.Int()))
+	body, err := c.getPage("https://gakujo.shizuoka.ac.jp/portal/report/student/searchList/search", data)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		body.Close()
+		_, _ = io.Copy(io.Discard, body)
+	}()
+	return io.ReadAll(body)
+}
+
+func (c *Client) fetchGeneralPurposeReportPage() ([]byte, error) {
 	data := url.Values{}
 	data.Set("headTitle", "レポート一覧")
 	data.Set("menuCode", "A02")
